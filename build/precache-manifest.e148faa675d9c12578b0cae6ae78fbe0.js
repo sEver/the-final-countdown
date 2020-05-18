355b44a0e0224d89f757a97dbd3bd677fe774901
sEver <sEver@users.noreply.github.com>
2020-05-19 01:14:23 +0200

    Fix build relative paths

---
+++ build/precache-manifest.e148faa675d9c12578b0cae6ae78fbe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba65ebe0b18fcc917c6c00c6378b36f",
+    "url": "/the-final-countdown/build/index.html"
+  },
+  {
+    "revision": "b1f1fd9dbb95bf10c0c0",
+    "url": "/the-final-countdown/build/static/css/main.27c4f017.chunk.css"
+  },
+  {
+    "revision": "b56d80eae04801fe27cf",
+    "url": "/the-final-countdown/build/static/js/2.ea1b3e83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-final-countdown/build/static/js/2.ea1b3e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f1fd9dbb95bf10c0c0",
+    "url": "/the-final-countdown/build/static/js/main.1cfb5388.chunk.js"
+  },
+  {
+    "revision": "bf13e49c83da9b3d109b",
+    "url": "/the-final-countdown/build/static/js/runtime-main.883a6ef5.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f08e2e76acef304a55a244f51f9c1dc.js"
+  "/the-final-countdown/build/precache-manifest.e148faa675d9c12578b0cae6ae78fbe0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/the-final-countdown/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
